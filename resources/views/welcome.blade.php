99f8993bcbc62f9806030b3216b158508fe12c15
AztFireSpell <alondigu@gmail.com>
2022-08-01 21:42:52 -0500

    add tailwind css and correction of delete tag:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,28 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <script src="https://cdn.tailwindcss.com"></script>
 
     </head>
-    <body>
-    <form action="tags" method="POST">
+    <body class="bg-gray-200 py-10">
+        <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+    <form action="tags" method="POST" class="flex mb-4">
         @csrf
-        <input type="text" name="name">
-        <input type="submit" value="Agregar">
+        <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="Nueva etiqueta...">
+        <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
     </form>
-    <h4>Listado de Etiquetas</h4>        
+    <h4 class="text-lg text-center mb-4">Listado de Etiquetas</h4>        
         <table>
             @forelse($tags as $tag)
             <tr>
-                <td>
+                <td class="border px-4 py-2">
                     {{$tag->name}}
                 </td>
-                <td>
-                    <form action="tags/{$tag->id}" method="POST">
+                <td class="px-4 py-2">
+                    <form action="tags/{{ $tag->id }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <input type="submit" value="Eliminar">
+                        <input type="submit" value="Eliminar" class="px-4 rounded bg-red-500 text-white">
                     </form>
                 </td>
             </tr>
@@ -38,5 +40,6 @@
             </tr>
             @endforelse
         </table>
+        </div>
     </body>
 </html>
